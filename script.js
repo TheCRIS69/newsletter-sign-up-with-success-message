d489b0c820be5b3f902f22d186c581073f9988e5
TheCRIS69 <thecris1337@gmail.com>
2023-10-12 12:46:36 +0300

    Added media queries

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const formStart = document.querySelector('.form-start');
 const formEnd = document.querySelector('.form-end');
 const subscribeButton = document.querySelector('.subscribe');
+const inputEmail = document.querySelector('input[type="email"]');
+const userEmail = document.querySelector('#email');
 
 subscribeButton.addEventListener('click', changeFormStatus);
 
 function changeFormStatus() {
     formStart.style.display = 'none';
     formEnd.style.display = 'grid';
+    userEmail.textContent = inputEmail.value;
+    userEmail.style.fontWeight = '700';
 }
