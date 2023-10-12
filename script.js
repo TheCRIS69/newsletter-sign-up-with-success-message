7be390ad3384ed32d0b5904e3fb568476c4fdb14
TheCRIS69 <thecris1337@gmail.com>
2023-10-12 18:46:49 +0300

    Added a required email pattern for the subscribe button

--- script.js
+++ script.js
@@ -3,12 +3,22 @@ const formEnd = document.querySelector('.form-end');
 const subscribeButton = document.querySelector('.subscribe');
 const inputEmail = document.querySelector('input[type="email"]');
 const userEmail = document.querySelector('#email');
+const emailRequiredMessage = document.querySelector('.email-required-message');
 
-subscribeButton.addEventListener('click', changeFormStatus);
+subscribeButton.addEventListener('click', changeFormStatus) 
 
-function changeFormStatus() {
-    formStart.style.display = 'none';
-    formEnd.style.display = 'grid';
-    userEmail.textContent = inputEmail.value;
-    userEmail.style.fontWeight = '700';
+function changeFormStatus(e) {
+    e.preventDefault()
+    const emailValue = inputEmail.value;
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    if (!emailPattern.test(emailValue)) {
+        emailRequiredMessage.style.display = 'block';
+    } else {
+        emailRequiredMessage.style.display = 'none';
+        formStart.style.display = 'none';
+        formEnd.style.display = 'grid';
+        userEmail.textContent = emailValue;
+        userEmail.style.fontWeight = '700';
+    }
 }
